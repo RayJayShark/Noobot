9aa581e5ec2c2f107b1b2871ca72cf78fc5de682
Eric Hardiman <hardimaneric@gmail.com>
2019-08-28 10:24:23 -0400

    Removed required voice for now playing command

--- commands/music/nowplaying.js
+++ commands/music/nowplaying.js
@@ -15,34 +15,30 @@ module.exports = class PlayCommand extends commando.Command {
   }
 
   async run(message) {
-    if (message.member.voiceChannel) {
-      if (!servers[message.guild.id]) {
-        servers[message.guild.id] = {};
-      }
-      const dbserver = await helper.retrieveServer(message.guild.id);
-      const queue = await helper.retrieveQueue(dbserver.id);
-      if (queue.songs.length === 0) {
-        message.channel
-          .send("There is nothing currently playing.")
-          .then(message => {
-            message.delete(5000);
-          });
-      } else {
-        const server = servers[message.guild.id];
-        const embed = new Discord.RichEmbed()
-          .setColor("#0099ff")
-          .addField(
-            `${queue.songs[0].get().title}`,
-            `${helper.convertSeconds(
-              Math.floor(server.dispatcher.time / 1000)
-            )} / ${helper.convertSeconds(
-              queue.songs[0].get().lengthSeconds
-            )} - [Link](${queue.songs[0].get().url})`
-          );
-        message.channel.send(embed).then(message => message.delete(5000));
-      }
+    if (!servers[message.guild.id]) {
+      servers[message.guild.id] = {};
+    }
+    const dbserver = await helper.retrieveServer(message.guild.id);
+    const queue = await helper.retrieveQueue(dbserver.id);
+    if (queue.songs.length === 0) {
+      message.channel
+        .send("There is nothing currently playing.")
+        .then(message => {
+          message.delete(5000);
+        });
     } else {
-      message.reply("You need to be in a voice channel.");
+      const server = servers[message.guild.id];
+      const embed = new Discord.RichEmbed()
+        .setColor("#0099ff")
+        .addField(
+          `${queue.songs[0].get().title}`,
+          `${helper.convertSeconds(
+            Math.floor(server.dispatcher.time / 1000)
+          )} / ${helper.convertSeconds(
+            queue.songs[0].get().lengthSeconds
+          )} - [Link](${queue.songs[0].get().url})`
+        );
+      message.channel.send(embed).then(message => message.delete(5000));
     }
   }
 };
