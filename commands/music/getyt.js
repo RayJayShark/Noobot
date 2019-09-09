87ccfc7dac9ea13391c81c3571eabdf4e232762c
Eric <hardimaneric@gmail.com>
2019-09-09 17:20:41 -0400

    Added GetYT command for Spotify single song links.

---
+++ commands/music/getyt.js
@@ -0,0 +1,28 @@
+const commando = require("discord.js-commando");
+const Discord = require("discord.js");
+const helper = require("../../helpers");
+
+module.exports = class GetYtCommand extends commando.Command {
+  constructor(client) {
+    super(client, {
+      name: "getyt",
+      aliases: ["yt"],
+      group: "music",
+      memberName: "getyt",
+      description: "Retrieves YouTube link from Single Spotify Song URL."
+    });
+  }
+  async run(message, args) {
+    if (args.includes("open.spotify.com/track/")) {
+      const url = await helper.getSpotifyUrl(args);
+      const message = new Discord.RichEmbed()
+        .setAuthor("YouTube URL Found!")
+        .setFooter(url)
+        .setColor("#FF0000");
+    } else {
+      message.channel
+        .send("This command is for Single Spotify Song URLs.")
+        .then(message => message.delete(5000));
+    }
+  }
+};
