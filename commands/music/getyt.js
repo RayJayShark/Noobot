b0b4e41c1c5bfcc798a4447d8e9fe8ffabf41d08
Eric Hardiman <eric.hardiman@parkmobile.io>
2020-01-06 10:03:26 -0500

    Fixed gyt

--- commands/music/getyt.js
+++ commands/music/getyt.js
@@ -14,8 +14,8 @@ module.exports = class GetYtCommand extends commando.Command {
   }
   async run(message, args) {
     if (args.includes("open.spotify.com/track/")) {
-      const url = await helper.getSpotifyUrl(args);
-      message.channel.send(url);
+      const {info: {uri}} = await helper.getSpotifyUrl(args);
+      message.channel.send(uri);
     } else {
       message.channel
         .send("This command is for Single Spotify Song URLs.")
