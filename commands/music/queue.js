20213d83d9c44cc55422b4617fd485a4199e9ff4
Eric Hardiman <hardimaneric@gmail.com>
2020-01-14 02:38:09 -0500

    Added some handling stuff to queue and helper

--- commands/music/queue.js
+++ commands/music/queue.js
@@ -1,7 +1,6 @@
 const commando = require("discord.js-commando");
-const Discord = require("discord.js");
-const helper = require("../../helpers");
 const models = require("../../models");
+const helper = require("../../helpers");
 
 module.exports = class QueueCommand extends commando.Command {
   constructor(client) {
@@ -19,14 +18,40 @@ module.exports = class QueueCommand extends commando.Command {
     const queue = await helper.retrieveQueue(server.id);
     queue.songs.shift();
 
-    if (command) {
-      switch (command) {
-        case "edit":
-          helper.createPagination(queue.songs, message, false, true);
-          break;
+    if (queue.songs.length) {
+      if (command) {
+        switch (command) {
+          case "edit":
+            helper.createPagination(queue.songs, message, false, true);
+            break;
+
+          case "clear":
+            if (!queue.songs.length) {
+              return;
+            }
+            const queueLengthBeforeClear = queue.songs.length;
+            queue.songs.forEach(song =>
+              models.SongQueue.destroy({
+                where: { queueId: queue.id, songId: song.id }
+              })
+            );
+            message.channel
+              .send(
+                `Successfully removed ${queueLengthBeforeClear} songs from Queue.`
+              )
+              .then(message => message.delete(5000));
+            break;
+
+          default:
+            break;
+        }
+      } else {
+        helper.createPagination(queue.songs, message);
       }
     } else {
-      helper.createPagination(queue.songs, message);
+      message.channel
+        .send("No songs in queue.")
+        .then(message => message.delete(5000));
     }
   }
 };
