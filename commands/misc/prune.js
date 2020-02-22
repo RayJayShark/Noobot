3103eb2254e65ea7f25d092c0c5cebb8fe76ea0d
Eric Hardiman <hardimaneric@gmail.com>
2020-02-21 21:44:14 -0500

    Added prune command.

---
+++ commands/misc/prune.js
@@ -0,0 +1,27 @@
+const commando = require("discord.js-commando");
+
+module.exports = class PruneCommand extends commando.Command {
+  constructor(client) {
+    super(client, {
+      name: "prune",
+      group: "misc",
+      memberName: "prune",
+      description: "Mass deletes 1-100 Messages at a time."
+    });
+  }
+
+  async run(message, args) {
+    let amountToPrune = 1;
+    if (parseInt(args) && parseInt(args) <= 100 && parseInt(args) >= 1) {
+      amountToPrune = parseInt(args);
+    }
+
+    if (parseInt(args) > 100) amountToPrune = 100;
+
+    if (isNaN(args)) return message.reply("Not a number.");
+
+    await message.channel
+      .fetchMessages({ limit: amountToPrune })
+      .then(messages => message.channel.bulkDelete(messages));
+  }
+};
